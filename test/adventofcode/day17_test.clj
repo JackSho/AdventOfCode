7d13024409b7ed2175008a67781d1fbffcd32e53
Xiao Jie <xiaojie@basecity.com>
2019-08-24 17:29:56 +0800

    day17

---
+++ test/adventofcode/day17_test.clj
@@ -0,0 +1,28 @@
+(ns adventofcode.day17-test
+  (:require [clojure.test :refer :all]
+            [adventofcode.day17 :refer :all]))
+
+;;http://adventofcode.com/2017/day/17/input
+
+(def input 335)
+
+(deftest day17-test
+
+  (testing "day17-test-part1"
+
+    (is (= (short-circuit 3 2) 1))
+    (is (= (short-circuit 3 4) 3))
+    (is (= (short-circuit 3 6) 1))
+    (is (= (short-circuit 3 8) 6))
+    (is (= (short-circuit 3 9) 5))
+    (is (= (short-circuit 3 2017) 638))
+    (is (= (short-circuit input 2017) 1282)))
+
+  (testing "day17-test-part2"
+
+    (is (= (after-zero 3 1) 1))
+    (is (= (after-zero 3 2) 2))
+    (is (= (after-zero 3 3) 2))
+    (is (= (after-zero 3 5) 5))
+    (is (= (after-zero 3 9) 9))
+    (is (= (after-zero input 50000000) 27650600))))
