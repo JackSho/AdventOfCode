790c6efae6c5c183ac26668a38b30b63be80c7d6
Xiao Jie <xiaojie@basecity.com>
2020-12-16 19:27:52 +0800

    更新 Clojure 版本

--- project.clj
+++ project.clj
@@ -2,9 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.7.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot adventofcode.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
